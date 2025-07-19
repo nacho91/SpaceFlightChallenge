70de1bb91b8165ecc0c7011302024bac6959b13f
Ignacio Oviedo <oviedoignacio91@gmail.com>
2025-07-19 14:10:10 -0300

    Initial Application and ui structure

---
+++ app/src/main/java/com/nacho/spaceflight/challenge/SpaceFlightChallengeApplication.kt
@@ -0,0 +1,8 @@
+package com.nacho.spaceflight.challenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpaceFlightChallengeApplication : Application() {
+}
---
+++ app/src/main/java/com/nacho/spaceflight/challenge/ui/detail/DetailScreen.kt
@@ -0,0 +1,14 @@
+package com.nacho.spaceflight.challenge.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = hiltViewModel(),
+    articleId: Int
+) {
+
+}
